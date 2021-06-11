5da074f75893f9752dffc9673a4352a98939e563
Rajat Gupta <rajat2411gupta@gmail.com>
2021-06-11 16:41:41 +0530

    routes

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-var fs = require('fs');
-var myApp = require('./myApp');
+const express = require('express');
+require("dotenv/config");
+const app = express();
 
-var express = require('express');
-var app = express();
+const mware = (req, res, next) => {
+  console.log("I'm a middleWare...");
+  next(req.method, req.path, req.ip);
+};
 
-app.use('/public', express.static(process.cwd() + '/public'));
-  
-app.route('/').get(function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const next = (method, path, ip) => {
+  console.log(`${method} ${path} - ${ip}`)
+};
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  mware(req, res, next);
+  res.sendFile(__dirname + '/views/index.html');
 })
 
-app.listen(process.env.PORT);
+app.get("/json", (req, res) => {
+  res.json({
+    "message": "Hello World",
+  });
+})
+
+app.get("/user", (req, res, next) => {
+  req.time = new Date().toDateString();
+  next(req.method, req.path, req.ip);
+}, (req, res) => {
+  res.json({time: req.time});
+})
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(mware);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on the port: ${PORT}.....`);
+});
